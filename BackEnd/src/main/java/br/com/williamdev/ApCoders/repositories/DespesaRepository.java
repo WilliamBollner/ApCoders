45cdb65941a704094285fd568a76607975a46dce
William Bollner <williambollner@hotmail.com>
2022-05-04 08:19:12 -0300

    Deletando pastas inúteis

--- BackEnd/src/main/java/br/com/williamdev/ApCoders/repositories/DespesaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.williamdev.ApCoders.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.williamdev.ApCoders.entities.DespesaUnidade;
-
-public interface DespesaRepository extends JpaRepository<DespesaUnidade, Long>{
-
-}
--- BackEnd/src/main/java/br/com/williamdev/ApCoders/repositories/InquilinoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.williamdev.ApCoders.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.williamdev.ApCoders.entities.Inquilino;
-
-public interface InquilinoRepository extends JpaRepository<Inquilino, Long>{
-
-}
